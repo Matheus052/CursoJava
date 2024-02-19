7d241f2e12ece8754e640d7d43f60202242bc8a6
mathe <mathe@Matheus>
2024-02-19 19:59:12 -0300

    Ola

---
+++ OrientaçãoObjetos/src/Objeto/Aluno.java
@@ -0,0 +1,86 @@
+package Objeto;
+
+public class Aluno {
+
+
+	String nome;
+	int idade;
+	String dataNascimento;
+	String rg;
+	String nomeMae;
+	String nomePai;
+	String nomeEscola;
+	double nota1;
+	double nota2;
+	double nota3;
+	double nota4;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+	
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if (media >= 70) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+}
---
+++ OrientaçãoObjetos/src/OrientaçãoObjeto/CursoJava.java
@@ -0,0 +1,33 @@
+package OrientaçãoObjeto;
+
+import Objeto.Aluno;
+
+public class CursoJava {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno();
+		
+		aluno1.setNome("Matheus");
+		aluno1.setIdade(19);
+		aluno1.setDataNascimento("14/10/2004");
+		aluno1.setNomeMae("Cristina");
+		aluno1.setNomePai("Andre");
+		aluno1.setNomeEscola("JDEV");
+		aluno1.setNota1(10);
+		aluno1.setNota2(10);
+		aluno1.setNota3(10);
+		aluno1.setNota4(100);
+		
+		System.out.println("Nome = " + aluno1.getNome());
+		System.out.println("Idade = " + aluno1.getIdade());
+		System.out.println("Data Nascimento = " + aluno1.getDataNascimento());
+		System.out.println("Nome Da Mãe = " + aluno1.getNomeMae());
+		System.out.println("Nome do Pai = " + aluno1.getNomePai());
+		System.out.println("Nome da escola = " + aluno1.getNomeEscola());
+		System.out.println("Media escolar = " + aluno1.getMediaNota());
+		System.out.println("Status de Aprovação = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		
+	}
+
+}
