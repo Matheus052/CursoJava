a418b7ad12cdba5c63b39774239a5f132946ece1
mathe <mathe@Matheus>
2024-02-19 22:22:22 -0300

    Aula 19/02/2024

--- OrientaçãoObjetos/src/OrientaçãoObjeto/CursoJava.java
+++ OrientaçãoObjetos/src/OrientaçãoObjeto/CursoJava.java
@@ -1,23 +1,36 @@
 package OrientaçãoObjeto;
 
+import javax.swing.JOptionPane;
+
 import Objeto.Aluno;
 
 public class CursoJava {
 
 public static void main(String[] args) {
-				
+	
+		String nome = JOptionPane.showInputDialog("QUAL O NOME DO ALUNO?");
+		String idade = JOptionPane.showInputDialog("QUAL A IDADE DO ALUNO?");
+		String dataNascimento = JOptionPane.showInputDialog("QUAL A DATA DE NASCIMENTO DO ALUNO?");
+		String nomeMae = JOptionPane.showInputDialog("QUAL O NOME DA MÃE DO ALUNO?");
+		String nomePai = JOptionPane.showInputDialog("QUAL O NOME DO PAI?");
+		String nomeEscola = JOptionPane.showInputDialog("QUAL O NOME DA ESCOLA?");
+		String nota1 = JOptionPane.showInputDialog("QUAL A NOTA1 ?");
+		String nota2 = JOptionPane.showInputDialog("QUAL A NOTA2 ?");
+		String nota3 = JOptionPane.showInputDialog("QUAL A NOTA3 ?");
+		String nota4 = JOptionPane.showInputDialog("QUAL A NOTA4 ?");
+			
 		Aluno aluno1 = new Aluno();
 		
-		aluno1.setNome("Matheus");
-		aluno1.setIdade(19);
-		aluno1.setDataNascimento("14/10/2004");
-		aluno1.setNomeMae("Cristina");
-		aluno1.setNomePai("Andre");
-		aluno1.setNomeEscola("JDEV");
-		aluno1.setNota1(10);
-		aluno1.setNota2(10);
-		aluno1.setNota3(10);
-		aluno1.setNota4(100);
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setNomeMae(nomeMae);
+		aluno1.setNomePai(nomePai);
+		aluno1.setNomeEscola(nomeEscola);
+		aluno1.setNota1(Double.parseDouble(nota1));
+		aluno1.setNota2(Double.parseDouble(nota2));
+		aluno1.setNota3(Double.parseDouble(nota3));
+		aluno1.setNota4(Double.parseDouble(nota4));
 	
 		System.out.println("Nome = " + aluno1.getNome());
 		System.out.println("Idade = " + aluno1.getIdade());
@@ -27,6 +40,7 @@ public static void main(String[] args) {
 		System.out.println("Nome da escola = " + aluno1.getNomeEscola());
 		System.out.println("Media escolar = " + aluno1.getMediaNota());
 		System.out.println("Status de Aprovação = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		
 	
 		}
 
