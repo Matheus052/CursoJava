7336447f931c6e4b251ac0448b1f8fc78576bd24
mathe <mathe@Matheus>
2024-02-28 22:05:03 -0300

    Aula 28/02/2024

--- OrientaçãoObjetos/src/Classes/Aluno.java
+++ OrientaçãoObjetos/src/Classes/Aluno.java
@@ -1,36 +1,46 @@
 package Classes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Aluno {
-	
+
 	String nome;
 	int idade;
 	String dataNascimento;
 	String nomeMae;
 	String nomePai;
 	String nomeEscola;
-	
-	private Disciplina disciplina = new Disciplina();
-	 
-	public void setDisciplina(Disciplina disciplina) {
-		this.disciplina = disciplina;
+
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+
 	}
-	public Disciplina getDisciplina() {
-		return disciplina;
+
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
 	public String getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(String dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
@@ -38,48 +48,64 @@ public class Aluno {
 	public String getNomeMae() {
 		return nomeMae;
 	}
+
 	public void setNomeMae(String nomeMae) {
 		this.nomeMae = nomeMae;
 	}
+
 	public String getNomePai() {
 		return nomePai;
 	}
+
 	public void setNomePai(String nomePai) {
 		this.nomePai = nomePai;
 	}
+
 	public String getNomeEscola() {
 		return nomeEscola;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNomeEscola(String nomeEscola) {
 		this.nomeEscola = nomeEscola;
 	}
 
 	public double getMediaNota() {
-		return (disciplina.getNota1() + disciplina.getNota2() + disciplina.getNota3() + disciplina.getNota4());
+		
+		double somaNota = 0.0;
+		
+		for (Disciplina disciplina : disciplinas) {
+			somaNota += disciplina.getNota();
+		}
+		
+		
+		return 0;
 	}
-	
+
 	public boolean getAlunoAprovado() {
 		double media = this.getMediaNota();
 		if (media >= 70) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 
-	
 	}
+
 	@Override
 	public String toString() {
 		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", nomeMae="
-				+ nomeMae + ", nomePai=" + nomePai + ", nomeEscola=" + nomeEscola + ", disciplina=" + disciplina + "]";
+				+ nomeMae + ", nomePai=" + nomePai + ", nomeEscola=" + nomeEscola + "]";
 	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(dataNascimento, disciplina, idade, nome, nomeEscola, nomeMae, nomePai);
+		return Objects.hash(dataNascimento, idade, nome, nomeEscola, nomeMae, nomePai);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -89,18 +115,9 @@ public class Aluno {
 		if (getClass() != obj.getClass())
 			return false;
 		Aluno other = (Aluno) obj;
-		return Objects.equals(dataNascimento, other.dataNascimento) && Objects.equals(disciplina, other.disciplina)
-				&& idade == other.idade && Objects.equals(nome, other.nome)
-				&& Objects.equals(nomeEscola, other.nomeEscola) && Objects.equals(nomeMae, other.nomeMae)
-				&& Objects.equals(nomePai, other.nomePai);
+		return Objects.equals(dataNascimento, other.dataNascimento) && idade == other.idade
+				&& Objects.equals(nome, other.nome) && Objects.equals(nomeEscola, other.nomeEscola)
+				&& Objects.equals(nomeMae, other.nomeMae) && Objects.equals(nomePai, other.nomePai);
 	}
-	
+
 }
-
-
-
-
-
-
-
-
--- OrientaçãoObjetos/src/OrientaçãoObjeto/CursoJava.java
+++ OrientaçãoObjetos/src/OrientaçãoObjeto/CursoJava.java
@@ -3,71 +3,67 @@ package OrientaçãoObjeto;
 import javax.swing.JOptionPane;
 
 import Classes.Aluno;
+import Classes.Disciplina;
 
 public class CursoJava {
 
-public static void main(String[] args) {
-	
+	public static void main(String[] args) {
+
 		String nome = JOptionPane.showInputDialog("QUAL O NOME DO ALUNO?");
 		String idade = JOptionPane.showInputDialog("QUAL A IDADE DO ALUNO?");
 		String dataNascimento = JOptionPane.showInputDialog("QUAL A DATA DE NASCIMENTO DO ALUNO?");
 		String nomeMae = JOptionPane.showInputDialog("QUAL O NOME DA MÃE DO ALUNO?");
 		String nomePai = JOptionPane.showInputDialog("QUAL O NOME DO PAI?");
 		String nomeEscola = JOptionPane.showInputDialog("QUAL O NOME DA ESCOLA?");
-		
+
 		String discliplina1 = JOptionPane.showInputDialog("QUAL A 1º DISCIPLINA?");
 		String nota1 = JOptionPane.showInputDialog("QUAL A NOTA 1 ?");
-		
+
 		String discliplina2 = JOptionPane.showInputDialog("QUAL A 2º DISCIPLINA?");
 		String nota2 = JOptionPane.showInputDialog("QUAL A NOTA 2 ?");
-		
+
 		String discliplina3 = JOptionPane.showInputDialog("QUAL A 3º DISCIPLINA?");
 		String nota3 = JOptionPane.showInputDialog("QUAL A NOTA 3 ?");
-		
+
 		String discliplina4 = JOptionPane.showInputDialog("QUAL A 4º DISCIPLINA?");
 		String nota4 = JOptionPane.showInputDialog("QUAL A NOTA 4 ?");
-			
+
 		Aluno aluno1 = new Aluno();
-		
+
 		aluno1.setNome(nome);
-		aluno1.setIdade(Integer.valueOf(idade)); /*"Transforma" int em string*/
+		aluno1.setIdade(Integer.valueOf(idade)); /* "Transforma" int em string */
 		aluno1.setDataNascimento(dataNascimento);
 		aluno1.setNomeMae(nomeMae);
 		aluno1.setNomePai(nomePai);
 		aluno1.setNomeEscola(nomeEscola);
-		
-		aluno1.getDisciplina().setNota1(Double.parseDouble(nota1));/*"Tranforma" double em string*/
-	
-		aluno1.getDisciplina().setNota2(Double.parseDouble(nota2));/*"Tranforma" double em string*/
-		
-		aluno1.getDisciplina().setNota3(Double.parseDouble(nota3));/*"Tranforma" double em string*/
-	
-		aluno1.getDisciplina().setNota4(Double.parseDouble(nota4));/*"Tranforma" double em string*/
-		
-		aluno1.getDisciplina().setDisciplina1(discliplina1);
-		aluno1.getDisciplina().setDisciplina2(discliplina2);
-		aluno1.getDisciplina().setDisciplina3(discliplina3);
-		aluno1.getDisciplina().setDisciplina4(discliplina4);
-		
-		System.out.println(aluno1);/*Descrição do objeto na memoria*/
+
+		Disciplina disciplina1 = new Disciplina();
+		disciplina1.setDisciplina("Banco de dados");
+		disciplina1.setNota(90);
+
+		aluno1.getDisciplinas().add(disciplina1);
+
+		Disciplina disciplina2 = new Disciplina();
+		disciplina2.setDisciplina("HTML");
+		disciplina2.setNota(80);
+
+		aluno1.getDisciplinas().add(disciplina2);
+
+		Disciplina disciplina3 = new Disciplina();
+		disciplina3.setDisciplina("JSCODE");
+		disciplina3.setNota(70);
+
+		aluno1.getDisciplinas().add(disciplina3);
+
+		Disciplina disciplina4 = new Disciplina();
+		disciplina4.setDisciplina("GITHUB");
+		disciplina4.setNota(80);
+
+		aluno1.getDisciplinas().add(disciplina4);
+
+		System.out.println(aluno1);/* Descrição do objeto na memoria */
 		System.out.println("Média Aluno = " + aluno1.getMediaNota());
 		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-	
-	/*EQUALS E HAHSCODE (DIFERENCIAR E COMPARAR OBJETOS)*/
 
-	/*Aluno aluno1 = new Aluno();
-	aluno1.setNome("Matheus");
-	
-	Aluno aluno2 = new Aluno ();
-	aluno2.setNome("Matheus");
-	
-	if (aluno1.equals(aluno2)) {
-		System.out.println("Alunos são iguais");
-	}else {
-		System.out.println("Alunos são diferentes");
-	}*/
-	
-}
 	}
-	
-
+}
